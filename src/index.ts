215a4b2e3bf6929745be23831b3d3ba2323be576
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-08-10 03:15:38 +0900

    fix: remove duplicate line

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ export function ianaToWindows(timezone: string): string | undefined {
 export function windowsToIana(timezone: string, territory: string = "001"): string | undefined {
   const tz = timezones.find(w => w.w === timezone);
   if (!tz) return undefined;
-  if (!tz) return undefined;
 
   const zone = tz.i.find(w => w.territory === territory);
   return zone ? zone.type[0] : undefined;
